1acc93f3bfc8413e1a7f0d00c605c61dd4b0aead
cedricve <cedric@verstraeten.io>
2025-04-25 17:25:30 +0000

    Refactor code structure for improved readability and maintainability

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MatrixRainingLetters } from "react-mdr";
+
+function App() {
+  return (
+    <React.Fragment>
+        <MatrixRainingLetters key="foo-bar" custom_class="m-0 p-0" />
+    </React.Fragment>
+);
+}
+
+export default App;
